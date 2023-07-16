9b2e0f93f8e3734747b94253f23aa92049eeccb5
Reuben James <20reuben.james@utcportsmouth.org>
2023-07-16 14:27:27 +0000

    Create method to hash a password

--- login.py
+++ login.py
@@ -1,4 +1,9 @@
 # ------------------------------------------------------------------------------
+# Standard Python library imports
+# ------------------------------------------------------------------------------
+import hashlib
+
+# ------------------------------------------------------------------------------
 # Project imports
 # ------------------------------------------------------------------------------
 import mysql_handler
@@ -14,3 +19,32 @@ connection = mysql_handler.Connection(
     schema=config.get("mysql schema"),
     host=config.get("mysql host")
 )
+
+# ------------------------------------------------------------------------------
+# Variables
+# ------------------------------------------------------------------------------
+_number_hash_passes = config.get("passwords number_hash_passes")
+_hashing_salt = config.get("passwords salt") # Stored in the config as binary
+_hashing_algorithm = config.get("passwords hashing_algorithm")
+
+# ------------------------------------------------------------------------------
+# Password hashing
+# ------------------------------------------------------------------------------
+def hash(password):
+    """
+    Method to hash the password, including a salt.
+
+    password -> string
+        The string that is to be hashed.
+
+    Returns a string.
+    """
+    print(_hashing_salt)
+    result = hashlib.pbkdf2_hmac(
+        _hashing_algorithm,
+        password.encode("utf-8"), # Needs to be in binary
+        _hashing_salt, # Salt needs to be in binary - stored as binary in config
+        _number_hash_passes # Recommended number of passes is 100,000
+    )
+
+    return result.hex() # Hash is returned as a hex string, so converts back
