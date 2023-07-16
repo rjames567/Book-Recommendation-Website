44da110839d76fafbbb811defef7f4d9b13b995c
Reuben James <20reuben.james@utcportsmouth.org>
2023-07-16 13:49:48 +0000

    Add docstrings

--- config.py
+++ config.py
@@ -7,21 +7,39 @@ import re
 # Custom exceptions
 # ------------------------------------------------------------------------------
 class ConfigInvalidDataForType (Exception):
+    """
+    Exception for when a value in the configuration file is specified, but
+    cannot be cast to the specified variable type.
+    """
     def __init__(self, datatype, value, line):
         message = f"Error at line {line}: '{value}' is not a valid {datatype}"
         super().__init__(message)
 
 class ConfigInvalidDataTypeError (Exception):
+    """
+    Exception for when a datatype is specified in the configuration file that is
+    not known to the Configuration class.
+    """
     def __init__(self, datatype, line):
         message = f"Error at line {line}: '{datatype}' datatype is not known"
         super().__init__(message)
 
 class ConfigIndentationError (Exception):
+    """
+    Exception for when there is an unexpected indent in the config file.
+
+    Note that missing indentation will not raise errors, as it is impossible to
+    tell whether that the lack of indentation is intentional.
+    """
     def __init__(self, line):
         message = f"Error at line {line}: Unexpected Indentation"
         super().__init__(message)
 
 class ConfigVariableNotFound (Exception):
+    """
+    Exception for when a variable is requested from the configuration file
+    associated with the configuration instance, but is not found in the file.
+    """
     def __init__(self, variable, file):
         message = f"Configuration file '{file}' does not contain the variable '{variable}'"
         super().__init__(message)
@@ -29,12 +47,43 @@ class ConfigVariableNotFound (Exception):
 # ------------------------------------------------------------------------------
 # Classes
 # ------------------------------------------------------------------------------
-class Config:
+class Configuration:
+    """
+    Configuration class which handles the loading and requesting of data from
+    the configuration file which is passed in.
+    """
     def __init__(self, filename):
+        """
+        Constructor for the Configuration class.
+
+        filename -> string
+            Specifies the configuration file. This can be either an absolute or
+            relative file path.
+
+        Does not have a return value
+        """
         self._filename = filename
         self.load()
 
     def _cast_to_type(self, datatype, value, line_num):
+        """
+        Method to convert a string to any specified datatype, using the acronym
+        which is in the configuration file.
+
+        datatype -> string
+            The acronym for the datatype that is included in the configuration
+            file
+
+        value -> string
+            The value that is to be cast to a different data type
+
+        line_num -> integer
+            The line number in the configuration file where the datatype is
+            specified
+
+        Returns a value, which is the same as that specified by the datatype
+        parameter.
+        """
         match datatype:
             case "int":
                 try:
@@ -67,21 +116,26 @@ class Config:
                 raise ConfigInvalidDataTypeError(datatype, line_num)
 
     def load(self):
+        """
+        Method to load the configuration file, and convert it into a dictionary.
+
+        Does not have a return value
+        """
         with open(self._filename, "r") as f:
             contents = f.readlines()
 
-        heirachy = {}
+        hierarchy = {}
 
         for line_num, line in enumerate(contents):
             heading_re = re.match("(\w+):\s*", line)
             if heading_re:
                 heading = heading_re.group(1)
-                heirachy[heading] = {}
+                hierarchy[heading] = {}
             else:
                 entry_re = re.match("\t|\s{4}(\w+)\s+([\w-]+)\s*:\s*(.+)", line)
                 if heading is not None:
                     if entry_re:
-                        heirachy[heading][entry_re.group(1)] = self._cast_to_type(
+                        hierarchy[heading][entry_re.group(1)] = self._cast_to_type(
                             entry_re.group(2),
                             entry_re.group(3),
                             line_num+1
@@ -91,16 +145,26 @@ class Config:
                 else:
                     external_re = re.match("(\w+)\s+([\w-]+)\s*:\s*(.+)", line)
                     if external_re:
-                        heirachy[external_re.group(1)] = self._cast_to_type(
+                        hierarchy[external_re.group(1)] = self._cast_to_type(
                             external_re.group(2),
                             external_re.group(3),
                             line_num+1
                         )
                     else:
                         raise ConfigIndentationError(line_num+1)
-        self._config = heirachy
+        self._config = hierarchy
 
     def get(self, query_string):
+        """
+        Method to get a specific variable from the configuration file.
+
+        query_string -> string
+            String to specify which variable to retrieve:
+                Header Variable
+                or
+                Variable
+            Note that it is case sensitive.
+        """
         query_arr = query_string.split()
         if query_arr[0] in self._config.keys():
             res = self._config[query_arr[0]]
