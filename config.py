a835d4960269691f36494c226b038a4eb15ca702
Reuben James <20reuben.james@utcportsmouth.org>
2023-07-16 13:29:20 +0000

    Add method to load config from file

--- config.py
+++ config.py
@@ -16,12 +16,18 @@ class ConfigInvalidDataTypeError (Exception):
         message = f"Error at line {line}: '{datatype}' datatype is not known"
         super().__init__(message)
 
+class ConfigIndentationError (Exception):
+    def __init__(self, line):
+        message = f"Error at line {line}: Unexpected Indentation"
+        super().__init__(message)
+
 # ------------------------------------------------------------------------------
 # Classes
 # ------------------------------------------------------------------------------
 class Config:
     def __init__(self, filename):
         self._filename = filename
+        self.load()
 
     def _cast_to_type(self, datatype, value, line_num):
         match datatype:
@@ -54,3 +60,37 @@ class Config:
                     raise ConfigInvalidDataForType(datatype, value, line_num)
             case _:
                 raise ConfigInvalidDataTypeError(datatype, line_num)
+
+    def load(self):
+        with open(self._filename, "r") as f:
+            contents = f.readlines()
+
+        heirachy = {}
+
+        for line_num, line in enumerate(contents):
+            heading_re = re.match("(\w+):\s*", line)
+            if heading_re:
+                heading = heading_re.group(1)
+                heirachy[heading] = {}
+            else:
+                entry_re = re.match("\t|\s{4}(\w+)\s+([\w-]+)\s*:\s*(.+)", line)
+                if heading is not None:
+                    if entry_re:
+                        heirachy[heading][entry_re.group(1)] = self._cast_to_type(
+                            entry_re.group(2),
+                            entry_re.group(3),
+                            line_num+1
+                        )
+                    else:
+                        heading = None
+                else:
+                    external_re = re.match("(\w+)\s+([\w-]+)\s*:\s*(.+)", line)
+                    if external_re:
+                        heirachy[external_re.group(1)] = self._cast_to_type(
+                            external_re.group(2),
+                            external_re.group(3),
+                            line_num+1
+                        )
+                    else:
+                        raise ConfigIndentationError(line_num+1)
+        self._config = heirachy
