6e209342e48336e60d5b28fb37fcc2a41a4dd2a9
Reuben James <20reuben.james@utcportsmouth.org>
2023-07-17 07:54:18 +0000

    Add docstring

--- environ_manipulation.py
+++ environ_manipulation.py
@@ -8,6 +8,18 @@ import re
 # ------------------------------------------------------------------------------
 class application:
     def add_target(environ):
+        """
+        Modifies the environ dictionary given when using WSGI, to contain the
+        target sub application, as dictated by the first part of the URI.
+
+        Example path: /application_name/sub_process
+            Adds TARGET_APPLICATION, with value application_name
+
+        Modifies the environ dictionary, and adds "TARGET_APPLICATION" to it.
+        This can be accessed directly, and does not need to be reassigned.
+
+        Does not have a return value
+        """
         path = environ["PATH_INFO"]
         application = re.match("/[a-z-]+/([a-z-]+)", path).group(1) # Should not
             # include dashes in result, but included so it does not break if it
