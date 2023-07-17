3379c8bd73f3acb5bbf85eaa6f6ee7e62629981a
Reuben James <20reuben.james@utcportsmouth.org>
2023-07-17 07:55:11 +0000

    Add inclusion of cases and underscores in regex

--- environ_manipulation.py
+++ environ_manipulation.py
@@ -21,7 +21,7 @@ class application:
         Does not have a return value
         """
         path = environ["PATH_INFO"]
-        application = re.match("/[a-z-]+/([a-z-]+)", path).group(1) # Should not
+        application = re.match("/[\w-]+/([\w-]+)", path).group(1) # Should not
             # include dashes in result, but included so it does not break if it
             # does.
         environ["TARGET_APPLICATION"] = application # Will change the dictionary
