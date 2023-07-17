5540724ed436c429ccd32167e9121d62e7417193
Reuben James <20reuben.james@utcportsmouth.org>
2023-07-17 08:02:44 +0000

    Add method to get subroutine

--- environ_manipulation.py
+++ environ_manipulation.py
@@ -26,3 +26,23 @@ class application:
             # does.
         environ["TARGET_APPLICATION"] = application # Will change the dictionary
             # passed.
+
+    def add_sub_target(environ):
+        """
+        Modifies the environ dictionary given when using WSGI, to contain the
+        target process within the sub application, as dictated by the second
+        part of the URI.
+
+        Example path: /application_name/sub_process
+            Adds APPLICATION_PROCESS, with value sub_process
+
+        Modifies the environ dictionary, and adds "APPLICATION_PROCESS" to it.
+        This can be accessed directly, and does not need to be reassigned.
+
+        Does not have a return value
+        """
+        path = environ["PATH_INFO"]
+        sub = re.match("/[\w-]+/[\w-]+/([\w-]+)", path).group(1) # Should not
+            # include dashes in result, but included so it does not break if it
+            # does.
+        environ["APPLICATION_PROCESS"] = sub
