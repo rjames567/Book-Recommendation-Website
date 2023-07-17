f6598f85dc462591bd6fce3ee9084f5a0cf8ee03
Reuben James <20reuben.james@utcportsmouth.org>
2023-07-17 14:12:16 +0000

    Create method to write to output file

--- logger.py
+++ logger.py
@@ -22,10 +22,19 @@ class Logging:
 
         now = datetime.datetime.now()
         start += ("-" * 80) + "\nNew session created: "
-        start += now.strftime("%d-%m-%Y %H:%M:%S") + "\n" + ("-" * 80)
+        start += now.strftime("%d-%m-%Y %H:%M:%S") + "\n" + ("-" * 80) + "\n"
 
         self._output_file = open(self._filepath + "output.log", method)
         self._output_file.write(start)
+
+    def output_message(self, message):
+        now = datetime.datetime.now()
+        string = "[" + now.strftime("%d-%m-%Y %H:%M:%S") + "] "
+        length = len(string)
+        new_message = [message[i:i+(80-length)] for i in range(0, len(message), 80 - length)]
+        print(new_message)
+        string += ("\n" + " " * length).join(new_message) + "\n"
+        self._output_file.write(string)
 
     def __del__(self):
         self._output_file.close()
