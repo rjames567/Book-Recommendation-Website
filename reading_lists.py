a6c2a6edf880366e34cac0c9ff171062c4971c93
Reuben James <20reuben.james@utcportsmouth.org>
2023-07-18 15:28:36 +0000

    Create method to get user's list names

--- reading_lists.py
+++ reading_lists.py
@@ -1,8 +1,9 @@
 # ------------------------------------------------------------------------------
 # Project imports
 # ------------------------------------------------------------------------------
-import mysql_handler
 import configuration
+import data_structures
+import mysql_handler
 
 # ------------------------------------------------------------------------------
 # Instantiating import classes
@@ -14,3 +15,19 @@ connection = mysql_handler.Connection(
     schema=config.get("mysql schema"),
     host=config.get("mysql host")
 )
+
+# ------------------------------------------------------------------------------
+# List names
+# ------------------------------------------------------------------------------
+def get_names(user_id):
+    res = connection.query(
+        """
+        SELECT list_name from reading_list_names
+        WHERE user_id={};
+        """.format(user_id)
+    ) # List of single element tuples
+    output_queue = data_structures.Queue()
+    for i in res:
+        output_queue.push(i[0]) # i is a single element tuple
+
+    return output_queue
