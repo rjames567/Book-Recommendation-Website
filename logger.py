9cda1c710465b65c5542ed7af2d66069fc2855c2
Reuben James <20reuben.james@utcportsmouth.org>
2023-07-26 17:14:45 +0100

    Add line length specification

--- logger.py
+++ logger.py
@@ -8,7 +8,8 @@ import datetime
 # Logging
 # ------------------------------------------------------------------------------
 class Logging:
-    def __init__(self, filepath="/tmp/", clear=True):
+    def __init__(self, filepath="/tmp/", clear=True, line_length=150):
+        self._line_length = line_length
         self._filepath = filepath
         self._clear = clear
         self._open()
@@ -22,10 +23,11 @@ class Logging:
             start = "\n\n"
 
         now = datetime.datetime.now()
-        start += ("-" * 80) + "\nNew session created: "
-        start += now.strftime("%d-%m-%Y %H:%M:%S") + "\n" + ("-" * 80) + "\n"
+        start += ("-" * self._line_length) + "\nNew session created: "
+        start += now.strftime("%d-%m-%Y %H:%M:%S") + "\n" + ("-" * self._line_length) + "\n"
 
         self._write(start)
+        self._method = "a+"
 
     def _write(self, message):
         with open(self._filepath + "output.log", self._method) as f:
@@ -36,6 +38,7 @@ class Logging:
         now = datetime.datetime.now()
         string = "[" + now.strftime("%d-%m-%Y %H:%M:%S") + "] "
         length = len(string)
-        new_message = [message[i:i + (80 - length)] for i in range(0, len(message), 80 - length)]
+        new_message = [message[i:i + (self._line_length - length)] for i in range(0, len(message),
+                                                                                  self._line_length - length)]
         string += ("\n" + " " * length).join(new_message) + "\n"
         self._write(string)
