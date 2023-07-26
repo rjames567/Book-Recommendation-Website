50f9c1b88df2a73fa484a73dcdad9c6454944c93
Reuben James <20reuben.james@utcportsmouth.org>
2023-07-26 21:31:48 +0100

    Remove test files

--- yourapplication.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-# from redmine.lighttpd.net/projects/lighttpd/wiki/Mod_fastcgi
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return environ['QUERY_STRING'] + environ['SCRIPT_NAME']
