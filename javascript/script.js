9088b89a61855e9d8444b3461713cb7f55462f0b
Reuben James <20reuben.james@utcportsmouth.org>
2023-08-07 20:14:49 +0100

    Fix issue with deleting review and stars not updating

--- javascript/script.js
+++ javascript/script.js
@@ -787,7 +787,8 @@ function assignReviewDeleteButtonHandler () {
             data: JSON.stringify({
                 "session_id": sessionID,
                 "review_id": $(existingReview).data("id")
-            }) // The user can only have one review of the book
+            }), // The user can only have one review of the book
+            complete: reloadCurrentPage; // Avoids the stars not updating. // TODO Make this better and more efficient
         }); // The response does not matter
     });
 }
