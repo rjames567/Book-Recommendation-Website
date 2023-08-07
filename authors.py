548044a0132b55404d8bd02b264c93ff0dffd1a8
Reuben James <20reuben.james@utcportsmouth.org>
2023-08-07 12:57:46 +0100

    Create method to follow author

--- authors.py
+++ authors.py
@@ -14,3 +14,12 @@ connection = mysql_handler.Connection(
     schema=config.get("mysql schema"),
     host=config.get("mysql host")
 )
+
+# ------------------------------------------------------------------------------
+# Follow Author
+# ------------------------------------------------------------------------------
+def follow_author(user_id, author_id):
+    connection.query("""
+        INSERT INTO author_followers (user_id, author_id)
+        VALUES ({user_id}, {author_id});
+    """.format(user_id=user_id, author_id=author_id))
