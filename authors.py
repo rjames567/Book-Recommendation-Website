51681c08f3cf80f43f5aee125e485747a43a6769
Reuben James <20reuben.james@utcportsmouth.org>
2023-08-07 12:51:17 +0100

    Create authors module

---
+++ authors.py
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------------
+# Project imports
+# ------------------------------------------------------------------------------
+import configuration
+import mysql_handler
+
+# ------------------------------------------------------------------------------
+# Instantiating import classes
+# ------------------------------------------------------------------------------
+config = configuration.Configuration("project_config.conf")
+connection = mysql_handler.Connection(
+    user=config.get("mysql username"),
+    password=config.get("mysql password"),
+    schema=config.get("mysql schema"),
+    host=config.get("mysql host")
+)
