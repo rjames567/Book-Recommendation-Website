8a9eb04c5bf1c431b81080c1e0716b47b8336e82
Reuben James <20reuben.james@utcportsmouth.org>
2023-08-09 17:44:08 +0100

    Remove debug catchall

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1592,29 +1592,26 @@ class DiaryHandler(Handler):
         }
 
     def get_entries(self):
-        try:
-            session_id = self.retrieve_get_parameters()["session_id"]  # Only has one parameter, so this is fine.
-            write_log("          Session ID: " + session_id, self._log)
-            user_id = sessions.get_user_id(session_id)
-            write_log("          User ID: " + str(user_id), self._log)
+        session_id = self.retrieve_get_parameters()["session_id"]  # Only has one parameter, so this is fine.
+        write_log("          Session ID: " + session_id, self._log)
+        user_id = sessions.get_user_id(session_id)
+        write_log("          User ID: " + str(user_id), self._log)
 
-            result = diaries.get_entries(user_id)
+        result = diaries.get_entries(user_id)
 
-            response = json.dumps(result)
+        response = json.dumps(result)
 
-            status = "200 OK"
+        status = "200 OK"
 
-            write_log("          Response: " + response, self._log)
-            write_log("          Status: " + status, self._log)
+        write_log("          Response: " + response, self._log)
+        write_log("          Status: " + status, self._log)
 
-            response_headers = [
-                ("Content-Type", "application/json"),
-                ("Content-Length", str(len(response)))
-            ]
+        response_headers = [
+            ("Content-Type", "application/json"),
+            ("Content-Length", str(len(response)))
+        ]
 
-            return response, status, response_headers
-        except Exception as e:
-            write_log(e, self._log)
+        return response, status, response_headers
 
 # -----------------------------------------------------------------------------
 # Error Handler
