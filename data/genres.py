09c91276297d8ea3994e6ba630a018c25c11b748
Reuben James <20reuben.james@utcportsmouth.org>
2023-08-13 12:23:01 +0100

    Add about to file

--- data/genres.py
+++ data/genres.py
@@ -2,10 +2,10 @@ import json
 
 with open("tags.json", "r") as f:
     with open("genres.sql", "w+") as k:
-        k.write("INSERT INTO genres (genre_id, name) VALUES\n")
+        k.write("INSERT INTO genres (genre_id, name, about) VALUES\n")
         for i, line in enumerate(f):
             if i != 0:
                 k.write(",\n")
             data = json.loads(line)
-            k.write('({id}, "{tag}")'.format(id=data["id"] + 1, tag=data["tag"].replace('"', "'")))
+            k.write('({id}, "{tag}", "This genre does not have an about")'.format(id=data["id"] + 1, tag=data["tag"].replace('"', "'")))
         k.write(";")
