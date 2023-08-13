ae27fd11fc3d1e5cfd515bca560ecfc0fd53a06c
Reuben James <20reuben.james@utcportsmouth.org>
2023-08-13 12:19:43 +0100

    Create method to insert data to file

---
+++ data/genres.py
@@ -0,0 +1,11 @@
+import json
+
+with open("tags.json", "r") as f:
+    with open("genres.sql", "w+") as k:
+        k.write("INSERT INTO genres (genre_id, name) VALUES\n")
+        for i, line in enumerate(f):
+            if i != 0:
+                k.write(",\n")
+            data = json.loads(line)
+            k.write('({id}, "{tag}")'.format(id=data["id"] + 1, tag=data["tag"].replace('"', "'")))
+        k.write(";")
