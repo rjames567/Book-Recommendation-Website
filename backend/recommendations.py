68697e2ad7b45038d9723525e0bde0927e8289ef
Reuben James <20reuben.james@utcportsmouth.org>
2023-08-16 22:15:52 +0200

    Create method to generate matrix from book genres

--- backend/recommendations.py
+++ backend/recommendations.py
@@ -11,6 +11,36 @@ import mysql_handler
 class Recommendations:
     def __init__(self, connection):
         self._connection = connection
+        self._available_genres = len(self._connection.query("""
+            SELECT genre_id FROM genres        
+        """))  # This is computed here as it is needed frequently.
+    
+    def gen_book_matrix(self, book_id):
+        book_genres = self._connection.query("""
+            SELECT genre_id,
+                match_strength
+            FROM book_genres
+            WHERE book_id={}
+        """.format(book_id))
+
+        print(book_genres)
+
+        print(self._available_genres)
+
+        matrix = ml_utilities.Matrix(n=1, m=self._available_genres)
+
+        for i in range(self._available_genres):
+            matrix[i][0] = 0  # Set all genres to having match strenth of 0.
+
+        # matrix.print()
+
+        for i in book_genres:
+            matrix[i[0] - 1][0] = i[1]  # First index is 0 as there is only 1 
+            # column - It is subtracted as MySQL IDs start at 1. Second index
+            # is the book id. The value is the match strength.
+        
+        return matrix
+
 
 # -----------------------------------------------------------------------------
 # File execution
