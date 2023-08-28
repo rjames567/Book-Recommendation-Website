4f250338757856b83ad52b23b2872b1ecc00491b
Reuben James <20reuben.james@utcportsmouth.org>
2023-08-28 18:31:45 +0100

    Switch matricies to vectors

--- backend/recommendations.py
+++ backend/recommendations.py
@@ -15,35 +15,29 @@ class Recommendations:
             SELECT genre_id FROM genres        
         """))  # This is computed here as it is needed frequently.
     
-    def gen_book_matrix(self, book_id):
+    def gen_book_vector(self, book_id):
         book_genres = self._connection.query("""
             SELECT genre_id,
                 match_strength
             FROM book_genres
             WHERE book_id={}
         """.format(book_id))
-
-        print(book_genres)
-
-        print(self._available_genres)
-
-        matrix = data_structures.Matrix(
-            n=1,
-            m=self._available_genres,
+        vector = data_structures.Vector(
+            dimensions=self._available_genres,
             default_value=0
         )
 
         for i in book_genres:
-            matrix[i[0] - 1][0] = i[1]  # First index is 0 as there is only 1 
+            vector[i[0] - 1] = i[1]  # First index is 0 as there is only 1 
             # column - It is subtracted as MySQL IDs start at 1. Second index
             # is the book id. The value is the match strength.
         
-        return matrix
+        return vector
 
-    def gen_user_matrix(self, user_id):
-        matrix = data_structures.Matrix(n=1, m=self._available_genres)
+    def gen_user_vector(self, user_id):
+        vector = data_structures.Vector(dimensions=self._available_genres)
         
-        user_genres = self._connection.query(x:="""
+        user_genres = self._connection.query("""
             SELECT genre_id,
                 match_strength
             FROM user_genres
@@ -52,24 +46,17 @@ class Recommendations:
         # needed does reduce generation speed, but reduces required storage,
         # and given that the speed is not essential, this is acceptable.
 
-        print(x)
-
-        print(user_genres)
-
-        print(self._available_genres)
-
-        matrix = data_structures.Matrix(
-            n=1,
-            m=self._available_genres,
+        vector = data_structures.Vector(
+            dimensions=self._available_genres,
             default_value=0
         )
 
         for i in user_genres:
-            matrix[i[0] - 1][0] = i[1]  # First index is 0 as there is only 1 
+            vector[i[0] - 1] = i[1]  # First index is 0 as there is only 1 
             # column - It is subtracted as MySQL IDs start at 1. Second index
             # is the book id. The value is the match strength.
 
-        return matrix
+        return vector
 
 
 # -----------------------------------------------------------------------------
