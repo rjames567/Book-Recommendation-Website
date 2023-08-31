7a506c36d83ec0f0a858e55e68c2efddefc27f55
Reuben James <20reuben.james@utcportsmouth.org>
2023-08-31 18:28:47 +0100

    Add debugging option to class instantiation

--- backend/logger.py
+++ backend/logger.py
@@ -4,30 +4,33 @@
 import datetime
 import json
 
+
 # ------------------------------------------------------------------------------
 # Logging
 # ------------------------------------------------------------------------------
 class Logging:
-    def __init__(self, filepath="/tmp/", clear=True, line_length=150):
+    def __init__(self, debugging=False, filepath="/tmp/", clear=True, line_length=150):
+        self._debugging = debugging
         self._line_length = line_length
         self._filepath = filepath
         self._clear = clear
         self._open()
 
     def _open(self):
-        if self._clear:
-            self._method = "w+"
-            start = ""
-        else:
+        if self._debugging:
+            if self._clear:
+                self._method = "w+"
+                start = ""
+            else:
+                self._method = "a+"
+                start = "\n\n"
+
+            now = datetime.datetime.now()
+            start += ("-" * self._line_length) + "\nNew session created: "
+            start += now.strftime("%d-%m-%Y %H:%M:%S") + "\n" + ("-" * self._line_length) + "\n"
+
+            self._write(start)
             self._method = "a+"
-            start = "\n\n"
-
-        now = datetime.datetime.now()
-        start += ("-" * self._line_length) + "\nNew session created: "
-        start += now.strftime("%d-%m-%Y %H:%M:%S") + "\n" + ("-" * self._line_length) + "\n"
-
-        self._write(start)
-        self._method = "a+"
 
     def _write(self, message):
         if type(message) == list:
@@ -41,11 +44,12 @@ class Logging:
             f.write(message)
 
     def output_message(self, message):
-        message = str(message)
-        now = datetime.datetime.now()
-        string = "[" + now.strftime("%d-%m-%Y %H:%M:%S") + "] "
-        length = len(string)
-        new_message = [message[i:i + (self._line_length - length)] for i in range(0, len(message),
-                                                                                  self._line_length - length)]
-        string += ("\n" + " " * length).join(new_message) + "\n"
-        self._write(string)
+        if self._debugging:
+            message = str(message)
+            now = datetime.datetime.now()
+            string = "[" + now.strftime("%d-%m-%Y %H:%M:%S") + "] "
+            length = len(string)
+            new_message = [message[i:i + (self._line_length - length)] for i in range(0, len(message),
+                                                                                    self._line_length - length)]
+            string += ("\n" + " " * length).join(new_message) + "\n"
+            self._write(string)
