a2e021259bdbedd0b7b35f88a15b41800f0babef
Reuben James <20reuben.james@utcportsmouth.org>
2023-09-03 14:06:30 +0100

    Add database connection

--- backend/information_retrieval.py
+++ backend/information_retrieval.py
@@ -9,3 +9,16 @@ import mysql_handler
 # -----------------------------------------------------------------------------
 def clean_data(string):
     return "".join([i.lower() for i in string if i.isalnum() or i == " "])
+
+
+# -----------------------------------------------------------------------------
+# File execution
+# -----------------------------------------------------------------------------
+if __name__ == "__main__":
+    config = configuration.Configuration("./project_config.conf")
+    connection = mysql_handler.Connection(
+        user=config.get("mysql username"),
+        password=config.get("mysql password"),
+        schema=config.get("mysql schema"),
+        host=config.get("mysql host")
+    )
