54af07b00772ae3b4b02f00b5dbe2884ca255f4f
Reuben James <20reuben.james@utcportsmouth.org>
2023-09-03 15:35:35 +0100

    Add method to generate unique words

--- backend/information_retrieval.py
+++ backend/information_retrieval.py
@@ -1,4 +1,9 @@
 # -----------------------------------------------------------------------------
+# Standard Python ibrary imports
+# -----------------------------------------------------------------------------
+import itertools
+
+# -----------------------------------------------------------------------------
 # Project imports
 # -----------------------------------------------------------------------------
 import configuration
@@ -12,6 +17,31 @@ def clean_data(string):
 
 
 # -----------------------------------------------------------------------------
+# Objects
+# -----------------------------------------------------------------------------
+class DocumentCollection:
+    def __init__(self, connection):
+        self._connection = connection
+    
+    def gen_unique_words(self):
+        words = list(itertools.chain(*[i[0].split(" ") for i in self._connection.query("SELECT clean_title FROM books")]))
+        words.append(list(itertools.chain(*[i[0].split() for i in self._connection.query("SELECT clean_name FROM authors")])))
+        words.append(list(itertools.chain(*[i[0].split() for i in self._connection.query("SELECT clean_name from genres")])))
+
+        unique_words = set(list(itertools.chain(*words)))
+
+        self._connection.query("DELETE FROM unique_words")
+        values = ""
+
+        for i in unique_words:
+            if i != "":
+                if values != "":
+                    values += ","
+                values += f'("{i}")'
+        
+        self._connection.query(x:=f"INSERT INTO unique_words (word) VALUES {values}")
+
+# -----------------------------------------------------------------------------
 # File execution
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
@@ -22,3 +52,6 @@ if __name__ == "__main__":
         schema=config.get("mysql schema"),
         host=config.get("mysql host")
     )
+
+    document = DocumentCollection(connection)
+    document.gen_unique_words()
