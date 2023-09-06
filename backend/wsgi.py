2af52ac8c7605089d3e72ee49268b6717194a68a
Reuben James <20reuben.james@utcportsmouth.org>
2023-09-06 18:06:43 +0100

    Add response to set new user preferences

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -843,7 +843,8 @@ class RecommendationsHandler(Handler):
         self._routes = {
             "get_recommendations": self.get_user_recommendations,
             "remove_recommendation": self.remove_user_recommendation,
-            "add_list_entry": self.add_to_want_read
+            "add_list_entry": self.add_to_want_read,
+            "set_user_preferences": self.set_new_user_preferences
         }
     
     def get_user_recommendations(self):
@@ -919,6 +920,32 @@ class RecommendationsHandler(Handler):
         recommendations.remove_stored_recommendation(user_id, book_id)
 
         response = "true" # The response does not matter - here for completeness only
+
+        status = "200 OK"
+
+        self._log.output_message("          Response: " + response)
+        self._log.output_message("          Status: " + status)
+
+        response_headers = [
+            ("Content-Type", "text/plain"),
+            ("Content-Length", str(len(response)))
+        ]
+
+        return response, status, response_headers
+
+    def set_new_user_preferences(self):
+        json_response = self.retrieve_post_parameters()
+        params = json.loads(json_response)
+        session_id = params["session_id"]
+        authors = params["authors"]
+        self._log.output_message(type(authors))
+        self._log.output_message("          Session ID: " + session_id)
+        user_id = sessions.get_user_id(session_id)
+        self._log.output_message("          User ID: " + str(user_id))
+        recommendations.set_user_initial_preferences(authors)
+        recommendations.recommend_user_books(user_id)
+
+        response = "true"  # The response does not matter - here for completeness only
 
         status = "200 OK"
 
