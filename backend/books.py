4f6daae21e8d85d38551b5498073b55353cf4dfd
Reuben James <20reuben.james@utcportsmouth.org>
2023-09-11 14:42:16 +0100

    Add method to get highly rated books

--- backend/books.py
+++ backend/books.py
@@ -360,6 +360,34 @@ class Books:
             rating_body=params["thoughts"]
         ))
         self._reading_lists.add_entry(user_id, list_id, book_id)
+    
+    def get_highly_rated(self):
+        res = self._connection.query("""
+            SELECT books.book_id,
+            books.title,
+            books.cover_image,
+            authors.first_name,
+            authors.surname,
+            authors.alias,
+                AVG(reviews.overall_rating) AS average_rating
+            FROM books
+            INNER JOIN authors ON books.author_id=authors.author_id
+            INNER JOIN reviews ON reviews.book_id=books.book_id
+            GROUP BY books.book_id
+            ORDER BY average_rating DESC;
+        """)
+
+        output_dict = dict()
+        for i, k in enumerate(res):
+            output_dict[i] = {
+                "author": authors_mod.names_to_display(k[3], k[4], k[5]),
+                "title": k[0],
+                "book_id": k[1],
+                "cover": k[2],
+            }
+
+        return output_dict[:self._number_summaries_home]  # The number of summaries on the genre
+        # page should be the same as the layout is the same
 
 
 # -----------------------------------------------------------------------------
