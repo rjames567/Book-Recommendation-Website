e038ea22cd99da1f880574913d2ae19d409952ed
Reuben James <20reuben.james@utcportsmouth.org>
2023-09-18 08:52:09 +0100

    Add function for comparison access

--- backend/searching_algorithms.py
+++ backend/searching_algorithms.py
@@ -1,13 +1,15 @@
-def binary_search(arr, target):
+def binary_search(arr, target, comparison_func=None):
+    if comparison_func is None:
+        comparison_func = lambda x: x
     # geeksforgeeks.org/python-program-for-binary-search/
     top = len(arr) - 1
     bottom = 0
 
     while bottom <= top:
         mid = (top + bottom) // 2
-        if arr[mid] < target:
+        if comparison_func(arr[mid]) > target:
             bottom = mid + 1
-        elif arr[mid] > target:
+        elif comparison_func(arr[mid]) < target:
             top = mid - 1
         else:
             return mid
