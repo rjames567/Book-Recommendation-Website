1233fa155c444db0b9ae99e1f7c2c440101fa473
Reuben James <20reuben.james@utcportsmouth.org>
2023-09-22 17:15:47 +0100

    Fix get highly rated method

--- backend/components/books.py
+++ backend/components/books.py
@@ -373,7 +373,7 @@ class Books:
             INNER JOIN authors ON books.author_id=authors.author_id
             INNER JOIN reviews ON reviews.book_id=books.book_id
             GROUP BY books.book_id
-            ORDER BY average_rating DESC;
+            ORDER BY average_rating DESC
             LIMIT {}
         """.format(self._number_summaries_home))  # The number of summaries on the genre
         # page should be the same as the layout is the same
