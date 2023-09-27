ad22873282db32dcd914ec8d2944eeb2a3c4f65c
Reuben James <20reuben.james@utcportsmouth.org>
2023-09-27 20:00:32 +0100

    Reduce query number for author about data

--- backend/components/authors.py
+++ backend/components/authors.py
@@ -44,12 +44,18 @@ class Authors:
     def get_about_data(self, author_id):
         res = self._connection.query("""
             SELECT authors.first_name,
-            authors.surname,
-            authors.alias,
-            authors.about,
-            (SELECT count(author_followers.user_id) FROM author_followers
-                WHERE author_followers.author_id=authors.author_id) AS followers 
+                authors.surname,
+                authors.alias,
+                authors.about,
+                (SELECT count(author_followers.user_id) FROM author_followers
+                    WHERE author_followers.author_id=authors.author_id) AS followers,
+                ROUND(AVG(reviews.overall_rating), 2) AS average,
+                COUNT(reviews.overall_rating) AS number
             FROM authors
+            LEFT OUTER JOIN books
+                ON authors.author_id=books.author_id
+            LEFT OUTER JOIN reviews
+                ON reviews.book_id=books.book_id
             WHERE authors.author_id={};
         """.format(author_id))
 
@@ -60,7 +66,7 @@ class Authors:
         else:
             res = res[0]
 
-        first_name, surname, alias, about, followers = res  # res is a 4 element tuple, so this unpacks it
+        first_name, surname, alias, about, followers, average_rating, number_ratings = res  # res is a 4 element tuple, so this unpacks it
         author = names_to_display(first_name, surname, alias)
         output_dict = {
             "name": author,
@@ -84,6 +90,9 @@ class Authors:
 
         output_dict["books"] = book_arr
 
+        output_dict["average_rating"] = float(average_rating)
+        output_dict["num_ratings"] = number_ratings
+
         genres = self._connection.query("""
             SELECT genres.name
             FROM genres
@@ -95,18 +104,6 @@ class Authors:
         """.format(author_id=author_id, match_strength=self._required_genre_match))
 
         output_dict["genres"] = [i[0] for i in genres]
-
-        avg_rating = self._connection.query("""
-            SELECT ROUND(AVG(reviews.overall_rating), 2) AS average,
-                   COUNT(reviews.overall_rating) AS number
-            FROM reviews
-            INNER JOIN books ON books.book_id=reviews.book_id
-            INNER JOIN authors ON authors.author_id=books.author_id
-            WHERE authors.author_id={}
-        """.format(author_id))[0]  # Get first (only) tuple from result
-
-        output_dict["average_rating"] = float(avg_rating[0])
-        output_dict["num_ratings"] = avg_rating[1]
 
         return output_dict
 
