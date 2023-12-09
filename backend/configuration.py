ef18417dfeb12ae0c3633bb8c6decbe2d9f9bb90
Reuben James <123649904+rjames567@users.noreply.github.com>
2023-12-09 19:48:26 +0000

    Add comments

--- backend/configuration.py
+++ backend/configuration.py
@@ -138,7 +138,7 @@ class Configuration:
 
         Does not have a return value
         """
-        with open(self._filepath, "r") as f:
+        with open(self._filepath, "r") as f:  # Implicitly closes the file after opening. Opens as read only
             contents = f.readlines()
 
         if self._default_filepath is not None:
@@ -148,32 +148,33 @@ class Configuration:
         else:
             hierarchy = {}
 
-        heading = ""
+        heading = ""  # No header as an empty string
         for line_num, line in enumerate(contents):
-            heading_re = re.match("(\w+):\s*", line)
-            if heading_re:
-                heading = heading_re.group(1).lower() + " "
+            heading_re = re.match("(\w+):\s*", line)  # Match headers in the line
+            if heading_re:  # Runs if there is a match to a heading
+                heading = heading_re.group(1).lower() + " "  # .lower ensures case insensitivity
             else:
-                entry_re = re.match("\t|\s{4}(\w+)\s+([\w-]+)\s*:\s*(.+)", line)
-                if heading != "":
+                entry_re = re.match("\t|\s{4}(\w+)\s+([\w-]+)\s*:\s*(.+)", line)  # Match indented records in line
+                if heading != "":  # If it is, it should be indented
                     if entry_re:
                         string = heading + entry_re.group(1)
                         hierarchy[string.lower()] = self._cast_to_type(
                             entry_re.group(2).lower(),
                             entry_re.group(3),
                             line_num + 1
-                        )
-                    else:
+                        )  # Change the datatype to the specified one
+                    else:  # There must be an unindented record, so it removes the header
                         heading = ""
                 if heading == "":
-                    external_re = re.match("(\w+)\s+([\w-]+)\s*:\s*(.+)", line)
+                    external_re = re.match("(\w+)\s+([\w-]+)\s*:\s*(.+)", line)  # Match unindented records in line
                     if external_re:
                         hierarchy[external_re.group(1).lower()] = self._cast_to_type(
                             external_re.group(2).lower(),
                             external_re.group(3),
-                            line_num + 1
+                            line_num + 1  # Increments the line number by 1 to give the line number of the exception    
                         )
-                    elif not (re.match("\s*", line)):
+                    elif not (re.match("\s*", line)):   # If there is no header, and there is non-space characters on the
+                        # line, it must be invalid indentation
                         raise ConfigIndentationError(line_num + 1)
         self._file_config = hierarchy
 
@@ -192,11 +193,11 @@ class Configuration:
         Returns the value stored in the specified variable, with the specified
         datatype.
         """
-        query_string = query_string.lower()
+        query_string = query_string.lower()  # Prevent case sensitivity
 
-        if query_string in self._file_config.keys():
+        if query_string in self._file_config.keys():  # Avoids needing the try-catch statement - is faster
             return self._file_config[query_string]
         else:
-            raise ConfigVariableNotFound(query_string, self._filepath)
+            raise ConfigVariableNotFound(query_string, self._filepath)  # Raises exception if it does not exist
 
 # Similar to YAML - but with more datatypes - binary strings
