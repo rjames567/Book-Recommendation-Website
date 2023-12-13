2a884bbbeef96d9eeead548b05fe9b63ed484ff8
Reuben James <123649904+rjames567@users.noreply.github.com>
2023-12-13 14:48:28 +0000

    Fix maintenance.py for new config

--- backend/maintenance.py
+++ backend/maintenance.py
@@ -10,17 +10,7 @@ import mysql_handler
 # -----------------------------------------------------------------------------
 # Project constants
 # -----------------------------------------------------------------------------
-config = configuration.Configuration("./project_config.conf")
-debugging = config.get("debugging")  # Toggle whether logs are shown
-number_hash_passes = config.get("passwords number_hash_passes")
-hashing_salt = config.get("passwords salt")  # Stored in the config as binary
-hashing_algorithm = config.get("passwords hashing_algorithm")
-token_size = config.get("session_id_length")
-genre_required_match = config.get("books genre_match_threshold")
-number_summaries_home = config.get("home number_home_summaries")
-number_similarities_about = config.get("home number_about_similarities")
-num_display_genres = config.get("home number_display_genres")
-num_search_results = config.get("search number_results")
+config = configuration.Configuration("./project_config.conf", default_conf_filename="./default_config.json")
 
 # -----------------------------------------------------------------------------
 # Database connection
@@ -35,12 +25,15 @@ connection = mysql_handler.Connection(
 # -----------------------------------------------------------------------------
 # Class instantiation
 # -----------------------------------------------------------------------------
-sessions = components.accounts.Sessions(connection, token_size)
+sessions = components.accounts.Sessions(
+    connection,
+    config.get("session_id_length")
+)
 recommendations = components.recommendations.Recommendations(
     connection,
     config.get("recommendations number_converge_iterations"),
     config.get("recommendations hyperparameter"),
-    config.get("home number_display_genres"),
+    config.get("number_display_genres"),
     config.get("recommendations inital_recommendation_matrix_value"),
     config.get("recommendations reading_list_percentage_increase"),
     config.get("recommendations author_following_percentage_increase"),
