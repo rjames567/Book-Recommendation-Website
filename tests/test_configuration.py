9959425a20adbd905f57c03c9f852d1a4b37d791
Reuben James <20reuben.james@utcportsmouth.org>
2024-01-29 12:15:48 +0000

    write integer tests

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -6,12 +6,37 @@ import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
 import backend.configuration as configuration
 
+
 class IntegerTest(unittest.TestCase):
-    def positive_integer(self):
-        config = configuration.Configuration("data_type_test.conf")
+    def test_positive_integer(self):
+        config = configuration.Configuration("tests/test_configurations/integer_positive.conf")
+        assert(config.get("value1") == 2)
+        assert(type(config.get("value1")) == int)
+
+        assert(config.get("value2") == 5678923423)
+        assert (type(config.get("value2")) == int)
+
+        assert(config.get("value3") == 10)
+        assert (type(config.get("value3")) == int)
+
+        assert(config.get("value4") == 567)
+        assert (type(config.get("value4")) == int)
+
+    def test_negative_integer(self):
+        config = configuration.Configuration("tests/test_configurations/integer_negative.conf")
+        assert (config.get("value1") == -2)
+        assert (type(config.get("value1")) == int)
+
+        assert (config.get("value2") == -5678923423)
+        assert (type(config.get("value2")) == int)
+
+        assert (config.get("value3") == -10)
+        assert (type(config.get("value3")) == int)
+
+        assert (config.get("value4") == -567)
+        assert (type(config.get("value4")) == int)
 
 if __name__ == '__main__':
     unittest.main()
