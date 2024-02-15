b7f5d88f3a626bd23227aabb2aa350298124a8f9
Reuben James <20reuben.james@utcportsmouth.org>
2024-02-15 15:10:00 +0000

    Add user 5 to test data

--- MySQL/test_data_recommendations_addition.sql
+++ MySQL/test_data_recommendations_addition.sql
@@ -1,3 +1,6 @@
+INSERT INTO users (first_name, surname, username, password_hash) VALUES
+("user", "5", "user5", "5d557544916fde5c6b162cfcbce84181fb2cbe8798439b643edf96ee4c5826b4");
+
 INSERT INTO recommendations (user_id, book_id, certainty) VALUES
 (1, 1, 0.5),
 (1, 2, 0.5),
