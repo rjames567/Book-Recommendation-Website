cc03c5087924c95571c462d29294245eefe15513
Reuben James <20reuben.james@utcportsmouth.org>
2024-03-03 21:25:46 +0000

    Remove secure from cookies

--- javascript/script.js
+++ javascript/script.js
@@ -254,7 +254,7 @@ $(".account-popups .window#sign-up form").on("submit", function (event) {
                     reloadCurrentPage();
 
                     if (remember) {
-                        $.cookie.set("sessionID", sessionID, { expires: 50, secure: true});
+                        $.cookie.set("sessionID", sessionID, { expires: 50 });
                     }
                 } else {
                     signUpAlert(result["message"]);
@@ -310,7 +310,7 @@ $(".account-popups .window#sign-in form").on("submit", function (event) {
                 hideAllSignPopups();
                 reloadCurrentPage();
                 if (remember) {
-                    $.cookie.set("sessionID", sessionID, { expires: 50, secure: true });
+                    $.cookie.set("sessionID", sessionID, { expires: 50 });
                 }
             } else {
                 signUpAlert(result["message"]);
